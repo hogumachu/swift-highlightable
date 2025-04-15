cee7548a0a1a49b5f77a36d757f5f54c8d7896c4
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-04-15 15:16:04 +0900

    Add example app

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by 홍성준 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
