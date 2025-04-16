2a398eead4c172bd928af08ddc9a970bac12585b
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-04-16 16:56:29 +0900

    wip

--- Sources/Highlightable/Markdown/Providers/Base/LinkRuleProvider.swift
+++ Sources/Highlightable/Markdown/Providers/Base/LinkRuleProvider.swift
@@ -11,7 +11,7 @@ public struct LinkRuleProvider: MarkdownRuleProvidable {
   
   // Matches Markdown links: [text](url)
   public static let regex = try! NSRegularExpression(
-    pattern: #"\[([^\]]+)\]\(([^)]+)\)"#,
+    pattern: #"\[(.*?)\]\((\S+?)(?:\s+"(.*?)")?\)"#,
     options: []
   )
   
@@ -20,7 +20,26 @@ public struct LinkRuleProvider: MarkdownRuleProvidable {
       regex: regex,
       textRules: [
         .underlineStyle(.single),
-        .foregroundColor(style.lighterColor) // TODO: - Add Color Dependency
+        .foregroundColor(style.lighterColor), // TODO: - Add Color Dependency
+        // TODO: - Setup title & link & description
+        .default(name: .link) { content, _ in
+          let matches = regex.matches(in: content, range: NSRange(content.startIndex..., in: content))
+          for match in matches {
+            let titleRange = Range(match.range(at: 1), in: content)
+            let urlRange = Range(match.range(at: 2), in: content)
+            let descriptionRange = Range(match.range(at: 3), in: content)
+            
+            let title = titleRange.map { String(content[$0]) } ?? ""
+            let url = urlRange.map { String(content[$0]) } ?? ""
+            let description = descriptionRange.map { String(content[$0]) } ?? ""
+            
+            print("Title: \(title)")
+            print("URL: \(url)")
+            print("Description: \(description)")
+            print("---")
+          }
+          return
+        }
       ]
     )
   }
